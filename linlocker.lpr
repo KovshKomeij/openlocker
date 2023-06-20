d8f31933bb553ea03193dbaac80082e354235bcc
dibil228of <85364286+dibil228of@users.noreply.github.com>
2023-06-20 11:12:15 +0300

    Add files via upload

---
+++ linlocker.lpr
@@ -0,0 +1,26 @@
+program linlocker;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, main
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Title:='openlocker';
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ main.pas
@@ -0,0 +1,79 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Panel1: TPanel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  code: string;
+  closeform: integer;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FormStyle := fsStayOnTop;
+  Visible := True;
+  code := '666228';
+  closeform := 1;
+  BorderStyle := bsNone;
+  Left := 0;
+  Top := 0;
+  Height := Screen.Height;
+  Width := Screen.Width;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  if closeform = 1 then
+     begin
+        CloseAction := caNone;
+     end
+  else
+     begin
+        CloseAction := caFree;
+     end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+   if Edit1.Text = code then
+      begin
+         closeform := 0;
+         Form1.Close;
+      end
+   else
+      begin
+         Label1.Caption := 'Код не верный!';
+      end;
+end;
+
+end.
+
